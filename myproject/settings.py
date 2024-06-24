3a9cbe938790ab2559ffbbc1e2deace1122ace58
K C Pawan <pawan@lentera.in>
2024-06-24 17:42:06 +0530

    initial commit

--- myproject/settings.py
+++ myproject/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-ylj)bt#zg&%!$w$pmqhi4t9gu%oh-05qfxyjom20r%=u8z*n49
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = [".awsapprunner.com","localhost","127.0.0.1:8000"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
